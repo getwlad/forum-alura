7c1ca6713d223b05696124b836c5fec04fd2b53a
getwlad <wladmir.rrodrigues@gmail.com>
2023-09-12 22:32:48 -0300

    iniciando projeto

---
+++ src/main/java/com/wladmirrodrigues/forumalura/ForumAluraApplication.java
@@ -0,0 +1,13 @@
+package com.wladmirrodrigues.forumalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumAluraApplication.class, args);
+	}
+
+}
