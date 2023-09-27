45bd24ae0eb7841a5f9605a7cd3c059d8e819d02
getwlad <wladmir.rrodrigues@gmail.com>
2023-09-26 23:24:58 -0300

    minor: adicionado documentacao sobre autenticacao no respostaController

--- src/main/java/com/wladmirrodrigues/forumalura/controller/RespostaController.java
+++ src/main/java/com/wladmirrodrigues/forumalura/controller/RespostaController.java
@@ -3,6 +3,7 @@ package com.wladmirrodrigues.forumalura.controller;
 import com.wladmirrodrigues.forumalura.domain.ValidacaoException;
 import com.wladmirrodrigues.forumalura.domain.resposta.*;
 import com.wladmirrodrigues.forumalura.domain.topico.TopicoRepository;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/topicos/{topicoId}/respostas")
+@SecurityRequirement(name = "bearer-key")
 public class RespostaController {
     @Autowired
     private RespostaService respostaService;
